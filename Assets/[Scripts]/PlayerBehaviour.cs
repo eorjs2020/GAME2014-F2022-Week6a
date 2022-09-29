7d11e6e379c6866da83db6e17e2583dd07ea9130
Default 101 <eorjs2020@gmail.com>
2022-09-29 08:54:29 -0400

    Initial

---
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    public Boundary boundary;
+    public float verticalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(0, verticalPosition);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //CheckBound();
+    }
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;  
+
+
+        transform.position += new Vector3(x, 0, 0);        
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, boundary.min, boundary.max) , verticalPosition);
+    }
+    void CheckBound()
+    {
+        if (transform.position.x > boundary.max)
+        {
+            transform.position = new Vector2(boundary.max, verticalPosition);
+        }
+        if (transform.position.x < boundary.min)
+        {
+            transform.position = new Vector2(boundary.min, verticalPosition);
+        }
+        
+    }
+}
---
+++ Assets/[Scripts]/StarBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarBehaviour : MonoBehaviour
+{
+    public float verticalSpeed;
+    public Boundary boundary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    public void Move()
+    {
+        transform.position -= new Vector3 (0, verticalSpeed * Time.deltaTime, 0);
+        
+    }
+
+    public void CheckBounds()
+    {
+        if(transform.position.y < boundary.min)
+        {
+            ResetStarts();
+        }
+    }
+
+    public void ResetStarts()
+    {
+        transform.position = new Vector2 (0, boundary.max);
+    }
+}
