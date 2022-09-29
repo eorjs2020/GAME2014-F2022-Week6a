5d465dfe53304e3ec9bedddba05de7a5d3f4b5a8
Default 101 <eorjs2020@gmail.com>
2022-09-29 09:24:00 -0400

    added simple touch input v1

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -8,37 +8,40 @@ public class PlayerBehaviour : MonoBehaviour
     public float speed = 2.0f;
     public Boundary boundary;
     public float verticalPosition;
-    // Start is called before the first frame update
+
+    public Camera cam;
+
     void Start()
     {
+        cam = Camera.main;
         transform.position = new Vector2(0, verticalPosition);
     }
     
     // Update is called once per frame
     void Update()
     {
+        MobileInput();
+        //ConventionalInput();
         Move();
-        //CheckBound();
+    }
+    
+    public void MobileInput()
+    {
+        foreach (var touch in Input.touches)
+        {
+            transform.position = cam.ScreenToWorldPoint(touch.position);
+        }
+    }
+
+    public void ConventionalInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        transform.position += new Vector3(x, 0, 0);
     }
     void Move()
-    {
-        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;  
-
-
-        transform.position += new Vector3(x, 0, 0);        
+    {             
         float clampPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, boundary.min, boundary.max) , verticalPosition);
     }
-  /*  void CheckBound()
-    {
-        if (transform.position.x > boundary.max)
-        {
-            transform.position = new Vector2(boundary.max, verticalPosition);
-        }
-        if (transform.position.x < boundary.min)
-        {
-            transform.position = new Vector2(boundary.min, verticalPosition);
-        }
-        
-    }*/
+ 
 }
